3f7bdf19247934b20240c8aaccfe82fde3309f1f
vophucthinh2006 <thinhvo2006vn@gmail.com>
2026-02-24 23:32:39 +0700

    DRIVER

--- Driver/Source/I2C.c
+++ Driver/Source/I2C.c
@@ -13,8 +13,8 @@ void I2C_Init(
 
 	switch (I2C_){
 	case I2C1:
-		GPIO_Config(I2C1_PORT_SCL, I2C1_PIN_SCL, GPIO_CNF_AF_OD, GPIO_MODE_OUT_10M);
-		GPIO_Config(I2C1_PORT_SDA, I2C1_PIN_SDA, GPIO_CNF_AF_OD, GPIO_MODE_OUT_10M);
+		GPIO_Config(I2C1_PORT_SCL, I2C1_PIN_SCL, GPIO_CNF_AF_OD, GPIO_MODE_OUT_50M);
+		GPIO_Config(I2C1_PORT_SDA, I2C1_PIN_SDA, GPIO_CNF_AF_OD, GPIO_MODE_OUT_50M);
 
 		CR1 = &I2C1_CR1;
 		CR2 = &I2C1_CR2;
@@ -22,8 +22,8 @@ void I2C_Init(
 		TRISE = &I2C1_TRISE;
 		break;
 	case I2C2:
-		GPIO_Config(I2C2_PORT_SCL, I2C2_PIN_SCL, GPIO_CNF_AF_OD, GPIO_MODE_OUT_10M);
-		GPIO_Config(I2C2_PORT_SDA, I2C2_PIN_SDA, GPIO_CNF_AF_OD, GPIO_MODE_OUT_10M);
+		GPIO_Config(I2C2_PORT_SCL, I2C2_PIN_SCL, GPIO_CNF_AF_OD, GPIO_MODE_OUT_50M);
+		GPIO_Config(I2C2_PORT_SDA, I2C2_PIN_SDA, GPIO_CNF_AF_OD, GPIO_MODE_OUT_50M);
 
 		CR1 = &I2C2_CR1;
 		CR2 = &I2C2_CR2;
@@ -32,8 +32,14 @@ void I2C_Init(
 		break;
 	default: return;
 	}
+	*CR1 |= (1 << 15);
+	*CR1 &= ~(1 << 15);
+
 	*CR1 &= ~(1 << 0);
 
+	*CR2 &= ~(0b11111 << 0);
+	*CR2 |= 8;
+	/*
 	*CR2 = (uint8_t)clock_frequency_MHz;
 
 	uint32_t clock_frequency_Hz = (clock_frequency_MHz * 1000000);
@@ -57,8 +63,9 @@ void I2C_Init(
 	}
 	*CCR = (uint32_t)(tHIGH_ns / TPCLK1_ns);
 	*TRISE = ((uint32_t)(tRISE_ns / TPCLK1_ns) + 1);
-
-	*CR1 |= (1 << 10);
+	*/
+	*CCR = 40;
+	*TRISE = 9;
 
 	*CR1 |= (1 << 0);
 }
@@ -83,7 +90,7 @@ void I2C_Start(
 		break;
 	default: return;
 	}
-	while (*SR2 & (1 << 1));
+	//while (*SR2 & (1 << 1));
 	*CR1 |= (1 << 8);
 
 	while(!(*SR1 & (1 << 0)));
@@ -110,20 +117,24 @@ void I2C_Stop(
 void I2C_Transmit_Address(
 		I2C_t I2C_,
 		uint8_t address,
+		uint8_t data_address,
 		I2C_LSB_t transmit_receive
 		){
 	volatile uint32_t *DR;
+	volatile uint32_t *CR1;
 	volatile uint32_t *SR1;
 	volatile uint32_t *SR2;
 
 	switch (I2C_){
 	case I2C1:
 		DR = &I2C1_DR;
+		CR1 = &I2C1_CR1;
 		SR1 = &I2C1_SR1;
 		SR2 = &I2C1_SR2;
 		break;
 	case I2C2:
 		DR = &I2C2_DR;
+		CR1 = &I2C2_CR1;
 		SR1 = &I2C2_SR1;
 		SR2 = &I2C2_SR2;
 		break;
@@ -131,13 +142,26 @@ void I2C_Transmit_Address(
 	}
 	*DR = (uint32_t)((address << 1) | (transmit_receive << 0));
 	while(!(*SR1 & (1 << 1)));
+
+	if (transmit_receive == I2C_LSB_RECEIVE){
+		*CR1 |= (1 << 10);
+	}
+
 	volatile uint8_t dummy = 0x00;
+	dummy = *SR1;
 	dummy = *SR2;
+
+	if (transmit_receive == I2C_LSB_TRANSMIT){
+	while(!(*SR1 & (1 << 7)));
+		*DR = data_address;
+	}
+	while(!(*SR1 & (1 << 2)));
 }
 
 void I2C_Master_Transmit_Data(
 		I2C_t I2C_,
-		uint8_t data
+		uint8_t *data,
+		uint8_t data_length
 		){
 	volatile uint32_t *DR;
 	volatile uint32_t *SR1;
@@ -153,14 +177,16 @@ void I2C_Master_Transmit_Data(
 		break;
 	default: return;
 	}
-	while (!(*SR1 & (1 << 7)));
-	*DR = data;
-	while (!(*SR1 & (1 << 2)));
+	for (int i = 0; i < data_length; i++){
+		while (!(*SR1 & (1 << 7)));
+		*DR = *data++;
+		while (!(*SR1 & (1 << 2)));
+	}
 }
 
 void I2C_Slave_Transmit_Data(
 		I2C_t I2C_,
-		uint8_t data
+		uint8_t *data
 		){
 	volatile uint32_t *DR;
 	volatile uint32_t *SR1;
@@ -178,7 +204,7 @@ void I2C_Slave_Transmit_Data(
 	}
 	while (!(*SR1 & (1 << 7)));
 
-	*DR = data;
+	*DR = *data++;
 
 	while (!(*SR1 & (1 << 10)) && !(*SR1 & (1 << 7)));
 	if (*SR1 & (1 << 10)){
@@ -238,31 +264,12 @@ uint8_t I2C_Master_Receive_Stop(
         break;
     default: return 0;
     }
-
-    while (!(*SR1 & (1 << 1)));
-    volatile uint32_t dummy;
-    dummy = *SR2;
-
     *CR1 &= ~(1 << 10);
 
+    I2C_Stop(I2C_);
     while (!(*SR1 & (1 << 6)));
 
-    uint8_t data = (uint8_t)(*DR);
-    I2C_Stop(I2C_);
-
-    return (uint8_t)data;
-}
-
-uint16_t I2C_Master_Receive_Data(
-		I2C_t I2C_,
-		I2C_ACK_t ack
-		){
-	if (ack == I2C_ACK_CONTINUE){
-		return I2C_Master_Receive_Continue(I2C_);
-	}
-	else {
-		return I2C_Master_Receive_Stop(I2C_);
-	}
+    return (uint8_t)*DR;
 }
 
 uint16_t I2C_Slave_Receive_Data(
