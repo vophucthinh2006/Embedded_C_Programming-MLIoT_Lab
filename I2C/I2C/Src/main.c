d0f0dddd5808449ce361b0f17b781d23d0eb22c8
vophucthinh2006 <thinhvo2006vn@gmail.com>
2026-02-24 23:30:15 +0700

    HOMEWORK

--- I2C/I2C/Src/main.c
+++ I2C/I2C/Src/main.c
@@ -1,39 +1,47 @@
 #include "RCC.h"
-#include "GPIO.h"
-#include "UART.h"
 #include "I2C.h"
 #include "ADXL345.h"
+#include "UART.h"
 
-int main (void){
+int16_t accel_x, accel_y, accel_z;
+char accel_str[20];
+
+uint8_t data_buffer[6];
+
+int main(void){
+	RCC_Enable(RCC_UART1);
+	RCC_Enable(RCC_I2C1);
 	RCC_Enable(RCC_PORTA);
 	RCC_Enable(RCC_PORTB);
-	RCC_Enable(UART1);
-	RCC_Enable(RCC_I2C1);
 
-	UART_Init(UART1, 9600, UART_WORD_LENGTH_8);
-	I2C_Init(I2C1, 8, I2C_SPEED_STANDARD);
+    UART_Init(UART1, 9600, UART_WORD_LENGTH_8);
 
-	int16_t X, Y, Z;
-	char x[20], y[20], z[20];
+    ADXL345_I2C_Init(I2C1);
+    while (1)
+    {
+        ADXL345_I2C_Receive_Values(ADXL345_DATAX0, data_buffer);
 
-	while(1){
-		ADXL345_I2C_Read(I2C1, &X, &Y, &Z);
-		value_to_str(X, x);
-		value_to_str(Y, y);
-		value_to_str(Z, z);
+        accel_x = (int16_t)((data_buffer[1] << 8) | data_buffer[0]);
 
-		UART_Transmit_String(UART1, "X: ");
-		UART_Transmit_String(UART1, x);
+        accel_y = (int16_t)((data_buffer[3] << 8) | data_buffer[2]);
+
+        accel_z = (int16_t)((data_buffer[5] << 8) | data_buffer[4]);
+
+        UART_Transmit_String(UART1, "Ox: ");
+        value_to_str(accel_x, accel_str);
+        UART_Transmit_String(UART1, accel_str);
+        UART_Transmit_String(UART1, "\r\n");
+
+        UART_Transmit_String(UART1, "Oy: ");
+        value_to_str(accel_y, accel_str);
+		UART_Transmit_String(UART1, accel_str);
 		UART_Transmit_String(UART1, "\r\n");
 
-		UART_Transmit_String(UART1, "Y: ");
-		UART_Transmit_String(UART1, y);
+		UART_Transmit_String(UART1, "Oz: ");
+		value_to_str(accel_z, accel_str);
+		UART_Transmit_String(UART1, accel_str);
 		UART_Transmit_String(UART1, "\r\n");
 
-		UART_Transmit_String(UART1, "Z: ");
-		UART_Transmit_String(UART1, z);
-		UART_Transmit_String(UART1, "\r\n");
-
-		for (int i = 0; i < 500000; i++);
-	}
+        for (int i = 0; i < 1000000; i++);
+    }
 }
